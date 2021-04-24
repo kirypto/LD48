feef9137cd8a29e7d313eb7342ef97dea62584bc
kirypto <kirypto@gmail.com>
2021-04-24 15:05:37 -0600

    Modify Projectile to launch at a specific speed when instantiated

--- Assets/Scripts/ProjectileScript.cs
+++ Assets/Scripts/ProjectileScript.cs
@@ -1,3 +1,26 @@
 using UnityEngine;
 
-public class ProjectileScript : MonoBehaviour { }
+public class ProjectileScript : MonoBehaviour
+{
+    [SerializeField] private float initialFiringForce = 1f;
+
+    private Rigidbody2D _rigidbody2D;
+    private Transform _transform;
+    private float _speed = 1f;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _transform = transform;
+    }
+
+    private void Start()
+    {
+        _rigidbody2D.AddForce(_transform.right * _speed * initialFiringForce);
+    }
+
+    public void Initialize(float speed = 1f)
+    {
+        _speed = speed;
+    }
+}
