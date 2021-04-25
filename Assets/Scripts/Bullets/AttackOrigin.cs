620106f17bc154875f405baede4333c767182fbb
kirypto <kirypto@gmail.com>
2021-04-24 22:58:33 -0600

    kc-enemy * Implemented placeholder attack pattern logic, currenly only logging would-be attacks

--- Assets/Scripts/Bullets/AttackOrigin.cs
+++ Assets/Scripts/Bullets/AttackOrigin.cs
@@ -1,6 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Bullets
 {
-    public class AttackOrigin : MonoBehaviour { }
+    public class AttackOrigin : MonoBehaviour
+    {
+        [SerializeField] private GameObject projectilePrefab;
+
+        private readonly IDictionary<string, int> _currentSteps = new Dictionary<string, int>();
+
+        private void Start()
+        {
+            BeginAttackPattern("cross");
+        }
+
+        private void BeginAttackPattern(string patternName)
+        {
+            _currentSteps[patternName] = 0;
+            StartCoroutine(AttackPatternLoop(patternName));
+        }
+
+
+        private IEnumerator AttackPatternLoop(string patternName)
+        {
+            while (true)
+            {
+                int currentStep = _currentSteps[patternName];
+                AttackStep attackStep = AttackPatterns.GetNextAttackStep(patternName, currentStep);
+                print($"Performing step of: {patternName}, firing {attackStep.ProjectileAttacks.Count}, " +
+                      $"delaying {attackStep.StepDelay} seconds.");
+
+                _currentSteps[patternName]++;
+                yield return new WaitForSeconds(attackStep.StepDelay);
+            }
+        }
+    }
 }
--- Assets/Scripts/Bullets/AttackPatterns.cs
+++ Assets/Scripts/Bullets/AttackPatterns.cs
@@ -1,4 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using AttackPattern = System.Collections.Generic.IList<Bullets.AttackStep>;
+
 namespace Bullets
 {
-    public static class AttackPatterns { }
+    public static class AttackPatterns
+    {
+        public static AttackStep GetNextAttackStep(string pattern, int stepNumber)
+        {
+            if (!_attackPatterns.ContainsKey(pattern))
+            {
+                // ReSharper disable once StringLiteralTypo
+                throw new ArgumentException($"Welp, you dun goofed. Ain't no such {pattern} pattern.");
+            }
+            AttackPattern attackPattern = _attackPatterns[pattern];
+            return attackPattern[stepNumber % attackPattern.Count];
+        }
+
+        private static readonly IDictionary<string, AttackPattern> _attackPatterns = new Dictionary<string, AttackPattern>
+        {
+                {
+                        "cross", new List<AttackStep>
+                        {
+                                new AttackStep(new List<ProjectileAttack>
+                                {
+                                        new ProjectileAttack(new Vector2(1f, 0f), "normal"),
+                                        new ProjectileAttack(new Vector2(-1f, 0f), "normal"),
+                                }, 2),
+                                new AttackStep(new List<ProjectileAttack>
+                                {
+                                        new ProjectileAttack(new Vector2(0f, 1f), "normal"),
+                                        new ProjectileAttack(new Vector2(0f, -1f), "normal"),
+                                }, 10),
+                        }
+                },
+        };
+    }
+
+    public readonly struct AttackStep
+    {
+        public IList<ProjectileAttack> ProjectileAttacks { get; }
+        public float StepDelay { get; }
+
+        public AttackStep(IList<ProjectileAttack> attackSteps, float stepDelay)
+        {
+            ProjectileAttacks = attackSteps.ToList();
+            StepDelay = stepDelay;
+        }
+    }
+
+    public readonly struct ProjectileAttack
+    {
+        public ProjectileAttack(Vector2 trajectory, string type) { }
+    }
 }
