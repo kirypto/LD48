4f881033f2e77646ddfb83052b72a35125167582
kirypto <kirypto@gmail.com>
2021-04-25 12:45:37 -0600

    kc-healthSystem * Tweak MainUIScript to log wave changes as well

--- Assets/Scripts/MainUIScript.cs
+++ Assets/Scripts/MainUIScript.cs
@@ -9,12 +9,16 @@ public class MainUIScript : MonoBehaviour
         if (playerHealth != null)
         {
             playerHealth.OnDamageTaken += playerHealthSystem => print($"Player took damage, now at {playerHealthSystem.Health}");
+            playerHealth.OnWaveDeath += playerHealthSys => print($"Enemy downed, is perma-dead? {playerHealthSys.IsPermaDead}, " +
+                                                                 $"future wave count: {playerHealthSys.FutureWaveCount}");
         }
 
         IHealthSystem enemyHealth = GameObject.FindWithTag("Enemy")?.GetComponent<IHealthSystem>();
         if (enemyHealth != null)
         {
             enemyHealth.OnDamageTaken += enemyHealthSys => print($"Enemy took damage, now at {enemyHealthSys.Health}");
+            enemyHealth.OnWaveDeath += enemyHealthSys => print($"Enemy downed, is perma-dead? {enemyHealthSys.IsPermaDead}, " +
+                                                               $"future wave count: {enemyHealthSys.FutureWaveCount}");
         }
     }
 }
