9cd0d278a3a5a49428c4140aee727bb68d18bfb0
kirypto <kirypto@gmail.com>
2021-04-25 12:34:00 -0600

    kc-healthSystem * Create placeholder MainUI to monitor player and enemy health

---
+++ Assets/Scripts/MainUIScript.cs
@@ -0,0 +1,20 @@
+﻿using Health;
+using UnityEngine;
+
+public class MainUIScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        IHealthSystem playerHealth = GameObject.FindWithTag("Player")?.GetComponent<IHealthSystem>();
+        if (playerHealth != null)
+        {
+            playerHealth.OnDamageTaken += playerHealthSystem => print($"Player took damage, now at {playerHealthSystem.Health}");
+        }
+
+        IHealthSystem enemyHealth = GameObject.FindWithTag("Enemy")?.GetComponent<IHealthSystem>();
+        if (enemyHealth != null)
+        {
+            enemyHealth.OnDamageTaken += enemyHealthSys => print($"Player took damage, now at {enemyHealthSys.Health}");
+        }
+    }
+}
