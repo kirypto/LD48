946bbca65c3f73f522de8fcb968f3aaaf1c3da4a
kirypto <kirypto@gmail.com>
2021-04-25 14:05:29 -0600

    kc-ui * Add Enemy health bar at top of screen

--- Assets/Scripts/MainUIScript.cs
+++ Assets/Scripts/MainUIScript.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using Health;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainUIScript : MonoBehaviour
 {
+    private Slider _healthSlider;
+
     private void Awake()
+    {
+        _healthSlider = transform.Find("Canvas").Find("EnemyHealthBar").GetComponent<Slider>();
+        _healthSlider.value = 1f;
+    }
+
+    private void Start()
     {
         IHealthSystem playerHealth = GameObject.FindWithTag("Player")?.GetComponent<IHealthSystem>();
         if (playerHealth != null)
         {
             playerHealth.OnDamageTaken += playerHealthSystem => print($"Player took damage, now at {playerHealthSystem.Health}");
-            playerHealth.OnWaveDeath += playerHealthSys => print($"Enemy downed, is perma-dead? {playerHealthSys.IsPermaDead}, " +
-                                                                 $"future wave count: {playerHealthSys.FutureWaveCount}");
+            playerHealth.OnWaveDeath += playerHealthSys => print(
+                    $"Enemy downed, is perma-dead? {playerHealthSys.IsPermaDead}, future wave count: {playerHealthSys.FutureWaveCount}");
+
         }
 
         IHealthSystem enemyHealth = GameObject.FindWithTag("Enemy")?.GetComponent<IHealthSystem>();
@@ -19,13 +29,18 @@ public class MainUIScript : MonoBehaviour
         {
             enemyHealth.OnDamageTaken += enemyHealthSys => print($"Enemy took damage, now at {enemyHealthSys.Health}");
             enemyHealth.OnWaveDeath += HandleEnemyWaveDeath;
+
+            enemyHealth.OnDamageTaken += heath =>
+            {
+                _healthSlider.value = heath.HealthPercentage;
+            };
         }
     }
 
     private void HandleEnemyWaveDeath(IHealthSystem enemyHealthSystem)
     {
         print($"Enemy downed, is perma-dead? {enemyHealthSystem.IsPermaDead}, " +
-                                       $"future wave count: {enemyHealthSystem.FutureWaveCount}");
+              $"future wave count: {enemyHealthSystem.FutureWaveCount}");
 
         print("freezing time");
         Time.timeScale = 0;
