93f45eaab1351ff1e41eaf876c3b4e8bc7a173df
kirypto <kirypto@gmail.com>
2021-04-25 10:35:02 -0600

    kc-healthSys * Create new IHealthSystem interface for player and enemy health management

---
+++ Assets/Scripts/Health/IHealthSystem.cs
@@ -0,0 +1,14 @@
+﻿namespace Health
+{
+    public interface IHealthSystem
+    {
+        public float Health { get; }
+        public float HealthMax { get; }
+        public float HealthPercentage { get; }
+        public void DealDamage(float damage);
+        public event OnDeathEvent OnDeath;
+
+
+        public delegate void OnDeathEvent();
+    }
+}
