ee1d5a09275451574f3fae101078b66b8ed4ce9f
kirypto <kirypto@gmail.com>
2021-04-25 11:36:09 -0600

    kc-healthSys * Implement HealthBehaviour class

--- Assets/Scripts/Health/HealthBehaviour.cs
+++ Assets/Scripts/Health/HealthBehaviour.cs
@@ -1,16 +1,49 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+
+using static InitializationUtils;
 
 namespace Health
 {
     public class HealthBehaviour : MonoBehaviour, IHealthSystem
     {
-        public float Health => throw new NotImplementedException();
-        public float HealthMax  => throw new NotImplementedException();
-        public float HealthPercentage  => throw new NotImplementedException();
+        [SerializeField] private List<float> healthWaves = new List<float>();
+
+        private int _currentWave;
+        private float _currentHealth;
+
+        private void Awake()
+        {
+            if (healthWaves.Count == 0)
+            {
+                StopAndThrowInitializationError($"Failed to initialize {name}, {nameof(healthWaves)} needs at least 1 value.");
+            }
+
+            _currentHealth = healthWaves[_currentWave];
+        }
+
+        public float Health => _currentHealth;
+        public float HealthMax => healthWaves[_currentWave];
+        public float HealthPercentage => HealthMax / Health;
         public void DealDamage(float damage)
         {
-            throw new NotImplementedException();
+            _currentHealth -= damage;
+            if (_currentHealth <= 0f)
+            {
+                HandleWaveDeath();
+            }
+        }
+
+        private void HandleWaveDeath()
+        {
+            OnDeath?.Invoke();
+            _currentWave += 1;
+
+            if (_currentWave < healthWaves.Count)
+            {
+                _currentHealth = healthWaves[_currentWave];
+            }
         }
 
         public event IHealthSystem.OnDeathEvent OnDeath;
