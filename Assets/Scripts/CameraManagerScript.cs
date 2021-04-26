7dc83d22d0a01b86bb3ce4e6429d9dc03a51e640
kirypto <kirypto@gmail.com>
2021-04-26 00:23:57 -0600

    Add CameraManager to disable boss cam on start

---
+++ Assets/Scripts/CameraManagerScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraManagerScript : MonoBehaviour
+{
+    [SerializeField] private GameObject playerCamera;
+    [SerializeField] private GameObject bossCamera;
+
+    private void Awake()
+    {
+        bossCamera.SetActive(false);
+    }
+}
+ 
